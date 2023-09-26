18f4a0cc154ae1a19be97320ad194280a2286435
IaDrake20 <ios.drake05@gmail.com>
2023-09-26 15:15:00 -0400

    collision isn't working, score is. Not pushing spawning just yet

---
+++ GameState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "gameState", menuName = "State/MyGameState")]
+public class GameState : ScriptableObject
+{
+    public static int score;
+    public static int browniePoints;
+}
